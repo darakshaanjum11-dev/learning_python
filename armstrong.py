c0a3f9acd6285cdfddd4f521a3963e4a94a90a94
darakshaanjum11-dev <darakshaanjum11@gmail.com>
2026-05-06 14:11:33 +0530

    armstrong

---
+++ armstrong.py
@@ -0,0 +1,16 @@
+
+# ----Armstrong----:-
+# n=int(input("enter any no:"))
+# x,y,td,sum=n,n,0,0
+# while n>0:
+#     td=td+1
+#     n=n//10
+# while x>0:
+#     ld=x%10
+#     sum=sum+ld**td
+#     x=x//10
+# if y==sum:
+#     print("armstrong:")
+# else:
+#     print("not armstrong:")
+
