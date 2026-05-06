0927bd4e5798d743000608afecc004df28dcbdc6
darakshaanjum11-dev <darakshaanjum11@gmail.com>
2026-05-06 14:14:06 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,40 @@
+# ---dictionary---its always target key
+# # dictionary
+# d={ 'name':'python':1:'java';5:'active':'python'}
+
+
+# dict
+# key must be unique
+# value may be duplicate
+# indexing not supported
+
+
+# # exp:--
+# d={'name':'dara',
+#    'age':19,
+#    'quali': 'bca'}
+# print(sum(d))  o/p error
+
+# d = {1:'dar',
+#      8:'here',
+#      9:'lol'}
+# print(sum(d))  o/p 18
+
+
+
+# ---dictionary---its always target key
+d={'x':10,'y':20,'z':'python'}
+# for i in d:
+# for i in d.keys():
+#     print(i) o/p x, y ,z
+    # print(d['x'])  o/p 10
+    # print(i,'=',d[i]) o/p x = 10  y = 20 z = python
+# for i in d.values():
+#     print(i) o/p 10 ,20,python
+# for i in d.items():
+#     print(i) o/p keys and its value but comma , sei separate horata hai means tuple kei form mein
+# for i,j in d.items():
+#     print(i,'=',j) o/p key and values but separated with = equale to
+# s={10,20,30,'python','java'}
+# for i in s:
+    # print(i) o/p print everthing but in unordered collection 
