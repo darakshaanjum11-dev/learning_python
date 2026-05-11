a6fec9becb813db4393508a88af36eaf9d2a6407
Daraksha Anjum <darakshaanjum11@gmail.com>
2026-05-11 12:56:29 +0530

    eval statement

---
+++ evalappend.py
@@ -0,0 +1,26 @@
+
+# # eval with append
+# l=eval(input("enter any list"))
+# element=eval(input("element :"))
+# l.append(element)
+# print(l)
+
+# # extend
+# l=eval(input("enter any list"))
+# element=eval(input("element :"))
+# l.append(element)
+# print(l)
+
+# # insert
+# l=eval(input("enter any list"))
+# element=eval(input("enter element"))
+# position=int(input("enter index position"))
+# l.insert(position,element)
+# print(l)
+
+# pop
+
+# l=eval(input("enter any list"))
+# element=eval(input("enter element"))
+# l.pop(element)
+# print(l)
