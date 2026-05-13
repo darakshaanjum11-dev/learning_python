ac6824bf47925ce2ee54e08b2c04a7bebffaa7bf
Daraksha Anjum <darakshaanjum11@gmail.com>
2026-05-13 12:46:30 +0530

    brake statements

---
+++ breakstatement.py
@@ -0,0 +1,19 @@
+#  break-----
+# n=int(input("enter no:-"))
+# i=1
+# while i<=n:
+#     if i==6:  #o/p here if we write 6 then its skip 6 and print all no expect 6
+#         break o/p just teminate loop same as pass
+#     else:
+#         print(i)
+#     i=i+1  
+
+# n=int(input("enter no:-"))
+# i=1
+# while i<=n:
+#     if i==6:  #o/p here if we write 6 then its skip 6 and print all no expect 6
+#         break #o/p just teminate loop same as pass
+#     else:
+#         print(i)
+#     i=i+1  
+# print("hello") #in this code it only terminate 6 and directly jum on hello skip 7,8
